44a3edc8bdd1e54de3df735ab9bdbe61fc040ace
shrikantkumar1999 <60041158+shrikantkumar1999@users.noreply.github.com>
2023-01-02 01:54:59 +0530

    Add files via upload

---
+++ precache-manifest.7a1b0b2414ee0b1300b20805423489d3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f2062dddae396779cce65d9e88ea53",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "8d92872249f4ed1b7cc5",
+    "url": "/portfolio2/static/css/main.33138350.chunk.css"
+  },
+  {
+    "revision": "78066731d1a8fc21740a",
+    "url": "/portfolio2/static/js/2.4f30fd93.chunk.js"
+  },
+  {
+    "revision": "e74be44262859c6bbcbe953b532630d6",
+    "url": "/portfolio2/static/js/2.4f30fd93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8d92872249f4ed1b7cc5",
+    "url": "/portfolio2/static/js/main.714f5b94.chunk.js"
+  },
+  {
+    "revision": "5178ef21f4b17c1b295b",
+    "url": "/portfolio2/static/js/runtime-main.725c372b.js"
+  },
+  {
+    "revision": "b82ec4ea99b656cb94d2be7bd0b2d1bb",
+    "url": "/portfolio2/static/media/codeforces.b82ec4ea.jpg"
+  },
+  {
+    "revision": "ea69a3b82520172805c412123d1c97db",
+    "url": "/portfolio2/static/media/error404.ea69a3b8.jpg"
+  },
+  {
+    "revision": "398f840c1638d3de3ba5918fc52d21e0",
+    "url": "/portfolio2/static/media/github.398f840c.png"
+  },
+  {
+    "revision": "c10d2ad6941208cfe8ec2f138b70e92f",
+    "url": "/portfolio2/static/media/me.c10d2ad6.jpg"
+  }
+]);
